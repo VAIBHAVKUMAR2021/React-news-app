c16b9df8a3382bb9df36dab8bbc7c1d961af866b
Vaibhav <kumarvaibhav8735@gmail.com>
2024-10-27 21:29:12 +0530

    abc

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 const API_DOMAIN = "https://gnews.io/api/v4/top-headlines?country=";
 const API_SEARCH_DOMAIN = "https://gnews.io/api/v4/search?q=";
-const API_KEY = process.env.REACT_APP_API_KEY;;
+const API_KEY = "2a1584a52a04d8a66d14b5149724dc18"
 export const endpointPath = (country, category) =>
-  `${API_DOMAIN}${country}&lang=en&category=${category}&apikey=${API_KEY}`;
+  `${API_DOMAIN}${country}&lang=en&category=${category}&apikey=2a1584a52a04d8a66d14b5149724dc18`;
 export const endpointSearch = (searchQuery) =>
-  `${API_SEARCH_DOMAIN}${searchQuery}&lang=en&apikey=${API_KEY}`;
+  `${API_SEARCH_DOMAIN}${searchQuery}&lang=en&apikey=2a1584a52a04d8a66d14b5149724dc18`;
